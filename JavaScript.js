2fc38f2f85293ffe7a89903e86f93827318c15af
Tyler Sperry <Tylrsperry@gmail.com>
2017-09-06 23:45:03 -0700

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,26 @@
+function main() {
+  $('.third').hide();
+  $('.third').fadeIn(4000);
+  $('.fourth').hide();
+  $('.fourth').fadeIn(5000);
+  $('.fifth').hide();
+  $('.fifth').fadeIn(6000);
+  $('.1').hide();
+  $('.1').fadeIn(4000);
+  $('.2').hide();
+  $('.2').fadeIn(5000);
+  $('.3').hide();
+  $('.3').fadeIn(6000);
+};
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(main);
